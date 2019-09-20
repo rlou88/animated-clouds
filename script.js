004a869c2d19f3756b5a882a4e5b961b6ba3f747
Ronald <ronald.lou88@gmail.com>
2019-09-20 10:15:59 -0500

    added event listener for mouse movement

--- script.js
+++ script.js
@@ -10,3 +10,18 @@ const colors = [
   'rgba(173,216,230,0.8)',
   'rgba(211,211,211,0.8)'
 ]
+const maxSize = 40
+const minSize = 0
+
+// mouse position
+let mouse = {
+  x: null,
+  y: null
+}
+window.addEventListener('mousemove',
+  function(event) {
+    mouse.x = event.x
+    mouse.y = event.y
+    console.log(mouse)
+  }
+)
